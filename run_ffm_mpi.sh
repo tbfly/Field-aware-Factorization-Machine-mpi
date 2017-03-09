e48e9244bc11a2c735d39eb486d3d5683cacf8f0
tbfly <tombowfly@gmail.com>
2017-03-09 16:39:05 +0800

    *): Update for CentOS7.2 runtime.

--- run_ffm_mpi.sh
+++ run_ffm_mpi.sh
@@ -12,5 +12,5 @@ process_number=6
 #mpirun -f ./hosts -np $process_number ./ffm_ftrl_mpi ftrl epoch=1000 batch_size=500 bias=0.0 alpha=0.1 beta=1.0 lambda1=0.001 lambda2=0.0 factor=2 group=68 isffm=0 isfm=1 islr=0 ./data/n2n_train ./data/n2n_test
 #mpirun -f ./hosts -np $process_number ./ffm_ftrl_mpi ftrl epoch=1000 batch_size=500 bias=0.0 alpha=0.1 beta=1.0 lambda1=0.001 lambda2=0.0 fea_dim=240000 factor=20 group=68 isffm=0 isfm=1 islr=0 ./data/v2v_train ./data/v2v_test --flagfile=config.h
 #mpirun -f ./hosts -np $process_number ./ffm_ftrl_mpi ftrl --flagfile=config.h
-mpirun --allow-run-as-root -np $process_number ./ffm_mpi ftrl --flagfile=config.h
+/usr/lib64/mpich/bin/mpirun -np $process_number ./ffm_mpi ftrl --flagfile=config.h
 
